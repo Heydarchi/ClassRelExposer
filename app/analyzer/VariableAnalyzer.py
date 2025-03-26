d7c54cd38333aab0d843a66361a8d5a857b87969
Mohammad Hossein Heydarchi <mhheidarchi2003@yahoo.com>
2025-03-26 19:44:31 +0100

    Improve documents (#8)
    * Update README
    Update setup.sh
    Update .gitignore
    * Reformat files by Black

--- app/analyzer/VariableAnalyzer.py
+++ app/analyzer/VariableAnalyzer.py
@@ -14,15 +14,15 @@ class VariableAnalyzer(AbstractAnalyzer):
         self.initPatterns()
 
     def initPatterns(self):
-        self.pattern[
-            FileTypeEnum.CPP
-        ] = "[\\s;\\n{}(::)]([a-zA-Z0-9_<>])+\\s+(\\*)?\\s?[a-zA-Z_,<>][a-zA-Z0-9_,<>]*\\s?[\\r\\n]?[;=]"
-        self.pattern[
-            FileTypeEnum.JAVA
-        ] = "[\\s;\\n{}}(::)].*[(public|private|protected)\\s+|(static)\\s+|(final)\\s+]?(([a-zA-Z0-9_<>])+::)?([a-zA-Z0-9_<>])+\\s+[a-zA-Z_,<>][a-zA-Z0-9_,<>]*\\s?[\\r\\n]?[;=]"
-        self.pattern[
-            FileTypeEnum.CSHARP
-        ] = "[\\s;\\n{}}(::)][(public|private|protected)\\s+|(static)\\s+|(final)\\s+|\(\s?]?(([a-zA-Z0-9_<>])+::)?([a-zA-Z0-9_<>])+\\s+[a-zA-Z_,<>][a-zA-Z0-9_,<>]*\\s?[\\r\\n]?[;=]"
+        self.pattern[FileTypeEnum.CPP] = (
+            "[\\s;\\n{}(::)]([a-zA-Z0-9_<>])+\\s+(\\*)?\\s?[a-zA-Z_,<>][a-zA-Z0-9_,<>]*\\s?[\\r\\n]?[;=]"
+        )
+        self.pattern[FileTypeEnum.JAVA] = (
+            "[\\s;\\n{}}(::)].*[(public|private|protected)\\s+|(static)\\s+|(final)\\s+]?(([a-zA-Z0-9_<>])+::)?([a-zA-Z0-9_<>])+\\s+[a-zA-Z_,<>][a-zA-Z0-9_,<>]*\\s?[\\r\\n]?[;=]"
+        )
+        self.pattern[FileTypeEnum.CSHARP] = (
+            "[\\s;\\n{}}(::)][(public|private|protected)\\s+|(static)\\s+|(final)\\s+|\(\s?]?(([a-zA-Z0-9_<>])+::)?([a-zA-Z0-9_<>])+\\s+[a-zA-Z_,<>][a-zA-Z0-9_,<>]*\\s?[\\r\\n]?[;=]"
+        )
 
     def analyze(self, filePath, lang, classStr=None):
         listOfVariables = list()
--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@ sudo apt install git python3.10 python-is-python3 graphviz python3-pip -y
 
 python -m pip install --upgrade pip 
 
+python3 -m venv venv
+
+source venv/bin/activate
+
 pip install -r requirements.txt
